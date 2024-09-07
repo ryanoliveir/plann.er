aa381550ad16c36ab2352e0c196ebac726ecd4ab
ryanoliveir <ryandehallo229@gmail.com>
2024-09-07 17:29:24 -0300

    add: list activities

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -1,44 +1,69 @@
 import { CircleCheck } from "lucide-react";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
-export function Activities(){
-    return (
-        <div className="space-y-8">
-            <div className="space-y-2.5">
-                <div className="flex gap-2 items-baseline">
-                    <span className="text-xl text-zinc-300 font-semibold">Dia 17</span>
-                    <span className="text-xs text-zinc-500">Sábado</span>
+interface Activity {
+  date: string;
+  activities: {
+    id: string;
+    title: string;
+    occurs_at: string;
+  }[];
+}
 
-                </div>
-                <span className="text-zinc-500 text-sm">Nenhuma atividade cadastrada nessa data.</span>
+export function Activities() {
+  const { tripId } = useParams();
+  const [activities, setActivities] = useState<Activity[]>([]);
+
+  console.log(activities);
+  useEffect(() => {
+    api
+      .get(`/trips/${tripId}/activities`)
+      .then((response) => setActivities(response.data.activities));
+  }, [tripId]);
+
+  return (
+    <div className="space-y-8">
+      {activities?.map((category, index) => {
+        return (
+          <div className="space-y-2.5" key={index}>
+            <div className="flex gap-2 items-baseline">
+              <span className="text-xl text-zinc-300 font-semibold">
+                Dia {format(category.date, "d")}
+              </span>
+              <span className="text-xs text-zinc-500">
+                {format(category.date, "EEEE", { locale: ptBR })}
+              </span>
             </div>
 
-
-            <div className="space-y-2.5">
-                <div className="flex gap-2 items-baseline">
-                    <span className="text-xl text-zinc-300 font-semibold">Dia 18</span>
-                    <span className="text-xs text-zinc-500">Domingo</span>
-
-                </div>
-                <div className="space-y-2.5">
-                    {/* <div className="px-4 py-2.5 h-10 bg-zinc-900 rounded-xl flex items-center justify-between"> */}
-                    <div className="px-4 py-2.5 h-10 bg-zinc-900 rounded-xl flex items-center gap-3">
-                      <CircleCheck className="size-5 text-lime-300"/>
-                      <span className="text-zinc-100">Academia em Grupo</span>
-                      <span className="ml-auto text-zinc-500 text-sm">08:00h</span>
+            {category.activities.length > 0 ? (
+              <div>
+                {category.activities.map((activity) => {
+                  return (
+                    <div className="space-y-2.5">
+                      {/* <div className="px-4 py-2.5 h-10 bg-zinc-900 rounded-xl flex items-center justify-between"> */}
+                      <div className="px-4 py-2.5 h-10 bg-zinc-900 rounded-xl flex items-center gap-3">
+                        <CircleCheck className="size-5 text-lime-300" />
+                        <span className="text-zinc-100">{activity.title}</span>
+                        <span className="ml-auto text-zinc-500 text-sm">
+                          {format(activity.occurs_at, 'HH:mm')}h
+                        </span>
+                      </div>
                     </div>
-
-                </div>
-                <div className="space-y-2.5">
-                    {/* <div className="px-4 py-2.5 h-10 bg-zinc-900 rounded-xl flex items-center justify-between"> */}
-                    <div className="px-4 py-2.5 h-10 bg-zinc-900 rounded-xl flex items-center gap-3">
-                      <CircleCheck className="size-5 text-lime-300"/>
-                      <span className="text-zinc-100">Academia em Grupo</span>
-                      <span className="ml-auto text-zinc-500 text-sm">08:00h</span>
-                    </div>
-
-                </div>
-            </div>
-
+                  );
+                })}
+              </div>
+            ) : (
+              <span className="text-zinc-500 text-sm">
+                Nenhuma atividade cadastrada nessa data.
+              </span>
+            )}
           </div>
-    )
+        );
+      })}
+    </div>
+  );
 }
--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,65 +1,109 @@
 import { X, Tag, Calendar } from "lucide-react";
 import { Button } from "../../components/button";
+import { FormEvent } from "react";
+import { useParams } from "react-router-dom";
+import { api } from "../../lib/axios";
+// import { DateRange, DayPicker } from "react-day-picker";
+// import "react-day-picker/style.css";
+// import { format } from "date-fns";
+
+
 
 interface CreateActivityModalProps {
-    closeActivityModal: () => void
-    
+  closeActivityModal: () => void;
 }
 
-export function CreateActivityModal({closeActivityModal}: CreateActivityModalProps) {
-    return (
-        <div className="fixed  inset-0 bg-black/60 flex items-center justify-center">
-        <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
-          <div className="space-y-2">
-            <div className="flex justify-between">
-              <h2 className="text-lg font-semibold">
-                Cadastrar atividade
-              </h2>
-              <button onClick={closeActivityModal}>
-                <X className="size-5 text-zinc-400" />
-              </button>
-            </div>
-            <p className="text-sm text-zinc-400">
-              Todos convidados podem visualizar as atividades.
-            </p>
-          </div>
+export function CreateActivityModal({
+  closeActivityModal,
+}: CreateActivityModalProps) {
+
+  const { tripId }  = useParams()
+  // const [isDatePickerOpen, setIsDatePickerOpen] = useState(false);
+  // const [selectedDate, setEventStartAndEndDates] = useState<DateRange | undefined>();
   
-          <form  className="space-y-3">
-            <div className="h-14 px-4  bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
-              <Tag className="size-4 text-zinc-400" />
+  // function openDatePicker() {
+  //   return setIsDatePickerOpen(true);
+  // }
+
+  // function closeDataPicker() {
+  //   return setIsDatePickerOpen(false);
+  // }
+
+  // const displayedDate = selectedDate && selectedDate.from && selectedDate.to ? 
+  // format(selectedDate.from, "d 'de' LLL").concat(' á ').concat(format(selectedDate.to, "d 'de' LLL")) : null;
+
+  function createActivity(event: FormEvent<HTMLFormElement>): void {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget)
+
+    const title = data.get('title')?.toString()
+    const date = data.get('starts_at')?.toString()
+    const time = data.get('time')?.toString()
+
+    const occurs_at = `${date}T${time}`
+
+    api.post(`/trips/${tripId}/activities`, {
+      title,
+      occurs_at
+    })
+
+    closeActivityModal()
+  }
+
+  return (
+    <div className="fixed  inset-0 bg-black/60 flex items-center justify-center">
+      <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+        <div className="space-y-2">
+          <div className="flex justify-between">
+            <h2 className="text-lg font-semibold">Cadastrar atividade</h2>
+            <button onClick={closeActivityModal}>
+              <X className="size-5 text-zinc-400" />
+            </button>
+          </div>
+          <p className="text-sm text-zinc-400">
+            Todos convidados podem visualizar as atividades.
+          </p>
+        </div>
+
+        <form onSubmit={createActivity} className="space-y-3">
+          <div className="h-14 px-4  bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
+            <Tag className="size-4 text-zinc-400" />
+            <input
+              className="text-zinc-400 bg-transparent outline-none flex-1"
+              placeholder="Qual é a ativade?"
+              type="text"
+              name="title"
+            />
+          </div>
+
+          <div className="flex items-center gap-2">
+            <div className="h-14 px-4 flex-1  bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
+              <Calendar className="size-4 text-zinc-400" />
               <input
-                className="text-zinc-400 bg-transparent outline-none flex-1"
-                placeholder="Qual é a ativade?"
-                type="text"
-                name="name"
+                className="text-zinc-400 bg-transparent outline-none flex-1 hide-date-indicator"
+                placeholder="Data"
+                type="date"
+                name="starts_at"
               />
             </div>
-
-            <div className="flex items-center gap-2">
-            <div className="h-14 px-4 flex-1  bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2">
-                <Calendar className="size-4 text-zinc-400" />
-                <input
-                  className="text-zinc-400 bg-transparent outline-none flex-1 hide-date-indicator"
-                  placeholder="Data"
-                  type="date"
-                  name="date"
-                />
-              </div>
-              <div className="h-14 w-36 px-4  bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2 ">
-                <Calendar className="size-4 text-zinc-400" />
-                <input
-                  className="text-zinc-400 bg-transparent outline-none flex-1 hide-clock-indicator"
-                  placeholder="Horário"
-                  type="time"
-                  name="time"
-                />
-              </div>
+            <div className="h-14 w-36 px-4  bg-zinc-950 border-zinc-800 rounded-lg flex items-center gap-2 ">
+              <Calendar className="size-4 text-zinc-400" />
+              <input
+                className="text-zinc-400 bg-transparent outline-none flex-1 hide-clock-indicator"
+                placeholder="Horário"
+                type="time"
+                name="time"
+              />
             </div>
-            <Button variant="primary" size="full">
-               Salvar Atividade
-            </Button>
-          </form>
-        </div>
+          </div>
+          <Button variant="primary" size="full">
+            Salvar Atividade
+          </Button>
+        </form>
       </div>
-    )
+    </div>
+  );
 }
+
+
